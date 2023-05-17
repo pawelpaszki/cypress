7a939ef3e72b3b3129ae51662bbd2f014ab6805e
Pawel Paszki <pawelpaszki@gmail.com>
2023-05-17 11:33:32 +0100

    wip: contact us test

---
+++ cypress/support/step_definitions/homepage_Steps.js
@@ -0,0 +1,13 @@
+import { Given, When } from "@badeball/cypress-cucumber-preprocessor";
+
+const url = "http://www.webdriveruniversity.com";
+
+Given(`I navigate to the webdriver university homepage`, () => {
+  cy.visit(url);
+})
+
+When(`I click on the contact us button`, () => {
+  cy.get("#contact-us")
+  .invoke("removeAttr", "target") // remove target to prevent from opening in new tab
+  .click()
+})
